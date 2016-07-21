85363588f74bd04852117e0b30b37d39cd6baecf
MHAHMA <mohamedhussen_1994@yahoo.com>
2016-07-21 09:09:31 +0200

    Add files via upload

---
+++ mod4_sol/script.js
@@ -0,0 +1,12 @@
+(function (window) {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i = 0; i < names.length; i++) {
+    var loNames = names[i].toLowerCase();
+    var firstLetter = loNames.charAt(0);
+    if (firstLetter === "j") {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})(window);
